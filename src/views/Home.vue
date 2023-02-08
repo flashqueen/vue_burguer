4bb53f201a6c939925edca1077f22a084a0e73af
Danilo Oliveira <dohkoweeds74@gmail.com>
2023-02-08 17:21:54 -0300

    projeto completo
    pedidos sendo realizados corretamente, alteração do status dos pedidos, deletar pedidos e site completo

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu sanduíche:</h1>
+    <BurguerForm />
+  </div>
+</template>
+
+<script>
+import Banner from '@/components/Banner.vue';
+import BurguerForm from '@/components/BurguerForm.vue'
+
+export default {
+  components: {
+    Banner,
+    BurguerForm
+  },
+  name: "Home"
+}
+</script>
